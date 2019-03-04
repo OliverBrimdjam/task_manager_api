d217bd19519064db40c02d7b14625c25c1a1ad6f
Igor Galvão de Britto <igor.galvao@gmail.com>
2019-03-04 15:42:20 +0000

    Criou usuario, index, show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/users", to: "users#index"
+  get "/tasks", to: "tasks#index"
+  get "/carros", to: "carrsos#index"
+  get "/coisas", to: "coisas#index"
+  get "/users/:id", to: "users#show"
 end
