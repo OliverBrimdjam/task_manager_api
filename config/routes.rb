c1acabee94a33427d916efcb1bb5e605bc8c262c
Igor Galvão de Britto <igor.galvao@gmail.com>
2019-03-04 14:57:47 +0000

    Rota para index de users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
-     
- end
+  get "/users", to: "users#index"
 end
