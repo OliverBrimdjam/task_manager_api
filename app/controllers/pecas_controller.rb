791f531ea33329a010411004c93ad777b37f66d1
Igor Galvão de Britto <igor.galvao@gmail.com>
2019-03-10 00:49:35 +0000

    fiz a rota, fiz controller, fiz o metodo capenga e só

---
+++ app/controllers/pecas_controller.rb
@@ -0,0 +1,5 @@
+class PecasController < ApplicationController
+  def veiculo
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get "/users", to: "users#index"
   get "/tasks", to: "tasks#index"
-  get "/carros", to: "carrsos#index"
-  get "/coisas", to: "coisas#index"
   get "/users/:id", to: "users#show"
   get "/task", to: "task#lista"
+  
+  get "/pecas/:identificador/veiculo", to: "pecas#veiculo"
 end
